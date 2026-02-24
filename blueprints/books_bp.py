b4342e97620188d6d63a18637bbc43fe11ca05ac
bazzebus <bazze91@hotmail.com>
2026-02-24 11:31:44 +0100

    Refactor book fetching and add error handling; create HTML template for book categories and JSON data for fantasy books

--- blueprints/books_bp.py
+++ blueprints/books_bp.py
@@ -1,10 +1,10 @@
 import os, requests, json, re
-from flask import Flask, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, render_template
 from bs4 import BeautifulSoup
 import datetime
 
 # Blueprint init
-books_bp = Blueprint('books_bp', __name__)
+books_bp = Blueprint('books_bp', __name__, template_folder='templates')
 
 # URL things
 BASE_URL = "https://books.toscrape.com/"
@@ -22,10 +22,14 @@ soup = BeautifulSoup(html_for_BASE_URL.text, 'html.parser')
 def page_turner(category_url):
     html_code = requests.get(category_url)
     soup_local = BeautifulSoup(html_code.text, 'html.parser')
-
-    current_page_html = soup_local.find('li', class_='current') # Fetches the HTML code 
-    current_page_text = current_page_html.get_text(strip=True)
-    current_page_str = re.findall(r'\b\d+\b', current_page_text) # Seperates the numbers from the text, stores each number as string in a list
+    try:
+        current_page_html = soup_local.find('li', class_='current') # Fetches the HTML code 
+        current_page_text = current_page_html.get_text(strip=True)
+        current_page_str = re.findall(r'\b\d+\b', current_page_text) # Seperates the numbers from the text, stores each number as string in a list
+    except:
+        current_page = 1
+        total_pages = 1
+        return current_page, total_pages
 
     # Converts the page numbers from strings to int
     # Seperates the current page from the total pages
@@ -59,8 +63,11 @@ def price_conversion(book_price):
     if not os.path.exists(f'{file_name}'):
         try:
             with open(file_name, 'w', encoding='utf-8') as txt_file:
-                txt = requests.get('https://www.forex.se/valuta/eur/').text
-                txt_file.write(txt)
+                html_local = requests.get('https://www.forex.se/valuta/eur/')
+                soup_local = BeautifulSoup(html_local.text, 'html.parser')
+                content = soup_local.find('span', class_='rate-example-list__example-list-item-to') # Finds converted price ONLY in the HTML
+                content = str(content) # Converts the BS4 tag to a text string
+                txt_file.write(content)
         except Exception as err:
             return('Unable to create TXT file.'), err
     try:
@@ -71,7 +78,6 @@ def price_conversion(book_price):
     
     # Parsing
     soup_stock_exchange = BeautifulSoup(stock_exchange, 'html.parser')
-    
     eur_to_sek = soup_stock_exchange.find('span', class_='rate-example-list__example-list-item-to').text # Finds the converted price: 1 EUR = XX,XX SEK
     sek_value = re.search(f'[0-9]+,[0-9]+', eur_to_sek).group() # Converts "XX,XX SEK" to "XX,XX"    
     price_converted = round(book_price * float(sek_value.replace(',', '.')), 2) # Calculates price in SEK
@@ -93,7 +99,7 @@ def fetch_html(URL):
     soup =BeautifulSoup(html_code.text, 'html.parser')
     return soup
     
-# --- Book Fetching !!! ---
+# --- Book Fetching !!! --- (Create)
 def scrape_book(URL): 
     #the scraped html code
     soup = fetch_html(URL)
@@ -116,7 +122,7 @@ def scrape_book(URL):
     book_price = float(book_price)
     converted_book_price = f'{price_conversion(book_price)} SEK'
 
-    return book_title, converted_book_price, book_rating, id #behöver lägga till price!!!!! //sebastian
+    return book_title, converted_book_price, book_rating, id
 
 def gather_book_data(category_url):
     try:
@@ -124,15 +130,19 @@ def gather_book_data(category_url):
 
         #scrape number of pages for the category
         current_page, max_page = page_turner(category_url)
-        while current_page != 2:#current_page != max_page:
+        while current_page <= max_page:
             # different codes depending on what URL stored in category_url
-            if re.search("index.html", category_url):
-                category_url = re.sub("index.html", f"page-{current_page}.html", category_url) #index.html -> page-X.html. used for multi-page category
-            elif re.search(f"page-{current_page-1}.html", category_url): #runs if the the URL has the same pagenumber as the last ran current_page varibable value
-                category_url = re.sub(f"page-{current_page-1}.html", f"page-{current_page}.html", category_url) #turns the page to the next 1 -> 2
+            if max_page > 1:
+                if re.search("index.html", category_url):
+                    category_url = re.sub("index.html", f"page-{current_page}.html", category_url) #index.html -> page-X.html. used for multi-page category                    
+                elif re.search(f"page-{current_page-1}.html", category_url): #runs if the the URL has the same pagenumber as the last ran current_page varibable value
+                    category_url = re.sub(f"page-{current_page-1}.html", f"page-{current_page}.html", category_url) #turns the page to the next 1 -> 2
 
             soup = fetch_html(category_url)
             books_html = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+            
+            print(category_url)
 
             #loops through all the books on ONE page and adds the data to a list of dictionaries.
             for book in books_html:
@@ -156,10 +166,9 @@ def gather_book_data(category_url):
             print(f"end of page {current_page}")
             current_page += 1 #changes what page to scrape
             #end of loop
-        return list_of_books
+        return list_of_books # WIP
     except Exception as e:
-        print(e)
-    return list_of_books # WIP
+        print("something went wrong!\n",e)
 
 # --- JSON Handling ---
 def load_json_file(file_name):
@@ -182,26 +191,34 @@ def save_books_to_json(category, category_url):
 
 
 # --- HTTP Methods ---
-# Route krävs, GET
-def get_books_by_category(category): # Temp name
-    # Ska kolla om lokal json fil finns och ladda den, annars skapa ny med webscraping. Returnerar alla böcker i kategorin.
-    # Kolla: Finns kategorin? om ja, Finns den lokalt? om nej, webscrape-a.
-    # Hämta sedan info om alla böcker utefter kategorin.
+# GET
+@books_bp.route('/books/<string:category>', methods=['GET'])
+def get_all_books_by_cat(category):
+    # Fetches all books by category
+    categories = get_categories()
+    category_part_url = None
+    # Checks for a valid category link
+    for link in categories:
+        if f'/{category}_' in link:
+            category_part_url = link
+            break
+    if category_part_url is None:
+        return jsonify({'error': f'Category {category} not found.'}), 404
+    
+    category_url = BASE_URL + category_part_url
+    
+    # Checks for local file, if it doesn't exist it webscrapes and stores the data in a new JSON file
+    file_name = dynamic_file_name(category)
+    if not os.path.exists(file_name):
+        save_books_to_json(category, category_url) # WIP
 
-    #attempt 1: //sebastian
-    try:
+    if not os.path.exists(file_name): # Checks again to make sure the file was created correctly
+        return jsonify({'error': 'Failed to create JSON file.'}), 500
+    
+    book_data = load_json_file(file_name)
+    return render_template('category_html.html', books=book_data)
         
-        if os.path.exists(f"../{category}_{datetime.datetime.now().strftime('%d%m%y')}"): #if the path with the category input with todays date
-            pass 
-        else:
-            #look if category exist
-            #scrape category of books
-            pass
-
-    except Exception as e:
-        print(e)
-        
-# Untested, needs function gather_book_data() to test. Assumes our route category is formatted 'fantasy', 'historical-fiction' etc.
+# GET ID
 @books_bp.route('/books/<string:category>/<string:id>', methods=['GET'])
 def get_book_by_id(category, id):
     # Fetches a book within a category by ID
@@ -227,9 +244,77 @@ def get_book_by_id(category, id):
         return jsonify({'error': 'Failed to create JSON file.'}), 500
     
     book_data = load_json_file(file_name)
+    temp_book_list = [] #needs a list to use html-template
     for book in book_data: # Searches for an ID match, returns book information
         if book['id'] == id:
             print('Book found!')
-            return jsonify(book)
-    
+            temp_book_list.append(book)
+            return render_template('category_html.html', books=temp_book_list)    
     return jsonify({'error': f'Book with ID/UPC {id} not found.'}), 404
+
+# POST
+@books_bp.route('/books/<string:category>', methods=['POST'])
+def create_book(category):
+    file_name = dynamic_file_name(category) # Checks for local file, if it doesn't exist it webscrapes and stores the data in a new JSON file
+
+    if not os.path.exists(file_name):
+        save_books_to_json(category)
+
+    data = load_json_file(file_name)
+
+    new_book = request.json # Expects a JSON object with the same structure as the existing books
+
+
+    required_fields = ['title', 'price', 'rating', 'id']
+    if not all(field in new_book for field in required_fields): # Checks if all required fields are present in the request JSON
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    data.append(new_book) # Adds the new book to the existing data list
+
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+    return jsonify(new_book), 201 # Returns the newly created book data as JSON with a 201 Created status code
+
+# Delete book from category
+@books_bp.route('/books/<string:category>/<string:id>', methods=['DELETE'])
+def delete_book(category, id):
+    try:
+        file_name = dynamic_file_name(category)#fetches the file name with datatype
+
+        #file doesn't exist
+        if not os.path.exists(file_name):
+            return jsonify({'message': f'local cache for {category} can not be found'}), 404
+        else:
+            with open(file_name, 'r', encoding='utf-8') as json_file:
+                json_data = json.load(json_file) #extracts a list of dictionaries (books)
+            for book in json_data:
+                if book['id'] == id: #if book id match with input id remove book from list
+                    json_data.remove(book)
+
+            #overwrite the file with the new list and return a confirmations message
+            with open(file_name, 'w', encoding='utf-8') as json_file:
+                json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+            return jsonify({'message':'book has been removed from cache'})
+
+    except Exception as e:
+        return jsonify({'something went wrong':e}), 500
+
+
+# Delete category
+@books_bp.route('/books/<string:category>', methods=['DELETE'])
+def delete_category(category):
+    file_name = dynamic_file_name(category) # Checks for local file
+
+    if not os.path.exists(file_name):
+        return jsonify({'error': f'Category {category} does not exist.'}), 404
+
+    try:
+        os.remove(file_name) # Deletes the JSON file associated with the category, effectively "deleting" the category data
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500 # Returns a 500 Internal Server Error with the exception message if file deletion fails
+
+    return jsonify({
+        'message': 'Category deleted successfully',
+        'deleted_category': category
+    }), 200
